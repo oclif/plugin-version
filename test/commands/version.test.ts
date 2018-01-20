681e3cd66ff10beb93819cc746cf4327e74fe121
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-19 22:29:12 -0800

    fix: update deps

--- test/commands/version.test.ts
+++ test/commands/version.test.ts
@@ -1,12 +1,8 @@
-import {describe, expect, it, output} from '@dxcli/dev-test'
-
-import cmd from '../../src/commands/version'
+import {testCommand} from '@dxcli/dev-test'
 
 const pjson = require('../../package.json')
 
-describe.stdout('version', () => {
-  it('shows version', async () => {
-    await cmd.run([])
-    expect(output.stdout).to.equal(`@dxcli/version/${pjson.version} (${process.platform}-${process.arch}) node-${process.version}\n`)
-  })
+describe('version', () => {
+  const stdout = `@dxcli/version/${pjson.version} (${process.platform}-${process.arch}) node-${process.version}\n`
+  testCommand(['version'], {stdout})
 })
--- test/hooks/init.test.ts
+++ test/hooks/init.test.ts
@@ -1,31 +1,6 @@
-import * as Config from '@dxcli/config'
-import {describe, expect, it, output} from '@dxcli/dev-test'
-import {inspect} from 'util'
-
-import init from '../../src/hooks/init'
+import {describe, testHook} from '@dxcli/dev-test'
 
 const pjson = require('../../package.json')
-
-export interface RunHookOptions {
-  description?: string
-  stdout?: string
-  stderr?: string
-  exit?: number
-}
-const testHook = (_: string, hookOpts: object = {}, opts: RunHookOptions = {}) => {
-  const description = opts.description || `run hook with opts: ${inspect(hookOpts)}`
-  let test = it
-  if (opts.stdout) test = test.stdout
-  if (opts.stderr) test = test.stderr
-  test(description, async () => {
-    const config = await Config.read({root: __dirname})
-    const run = () => init({config, ...hookOpts} as any)
-    if (typeof opts.exit === 'number') await expect(run()).to.be.rejectedWith(`EEXIT: ${opts.exit}`)
-    else await run()
-    if (opts.stdout) expect(output.stdout).to.equal(opts.stdout)
-    if (opts.stderr) expect(output.stderr).to.equal(opts.stderr)
-  })
-}
 
 describe('hooks:init', () => {
   const stdout = `@dxcli/version/${pjson.version} (${process.platform}-${process.arch}) node-${process.version}\n`
