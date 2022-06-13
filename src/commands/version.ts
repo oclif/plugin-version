b47a1fd83cc09f179e1594e94291051a043caaa8
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2022-06-13 10:53:14 -0400

    feat: allow getting more info from version command

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,7 +1,39 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
+import {type as osType, release as osRelease} from 'node:os'
 
 export default class Version extends Command {
+  public static flags = {
+    verbose: Flags.boolean({
+      summary: 'Show additional information about the CLI.',
+      description: 'Additionally shows the architecture, node version, operating system, and versions of plugins that the CLI is using.',
+    }),
+  }
+
   async run(): Promise<void> {
-    process.stdout.write(this.config.userAgent + '\n')
+    const {flags} = await this.parse(Version)
+    let output = `${this.config.userAgent} \n`
+
+    if (flags.verbose) {
+      const versions = this.config.userAgent.split(' ')
+      const cliVersion: string = versions[0]
+      const architecture: string = versions[1]
+      const nodeVersion: string = versions[2]
+
+      const pluginVersions: string = this.config.plugins.reduce((accumulator, plugin) => {
+        accumulator += `\n\t${plugin.name}@${plugin.version} (${plugin.type})`
+        return accumulator
+      }, '')
+
+      const osVersion = `${osType()} ${osRelease()}`
+
+      output = ` CLI Version : \n\t${cliVersion}
+\n Architecture: \n\t${architecture}
+\n Node Version : \n\t${nodeVersion}
+\n Plugin Version: ${pluginVersions}
+\n OS and Version: \n\t${osVersion}
+`
+    }
+
+    this.log(output)
   }
 }
