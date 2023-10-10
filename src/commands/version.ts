e88196134600e1aa1ccc9ebc27d57cb60de384ce
Mike Donnalley <mdonnalley@salesforce.com>
2023-10-10 10:54:53 -0600

    feat!: migrate to ESM
    BREAKING CHANGES: node 18 minimum, removed exports

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,9 +1,8 @@
 import {Command, Flags, Interfaces} from '@oclif/core'
-// eslint-disable-next-line unicorn/prefer-node-protocol
-import {EOL} from 'os'
+import {EOL} from 'node:os'
 
 export type VersionDetail = Omit<Interfaces.VersionDetails, 'pluginVersions'> & {
-  pluginVersions?: string[];
+  pluginVersions?: string[]
 }
 
 export default class Version extends Command {
@@ -11,8 +10,9 @@ export default class Version extends Command {
 
   public static flags = {
     verbose: Flags.boolean({
+      description:
+        'Additionally shows the architecture, node version, operating system, and versions of plugins that the CLI is using.',
       summary: 'Show additional information about the CLI.',
-      description: 'Additionally shows the architecture, node version, operating system, and versions of plugins that the CLI is using.',
     }),
   }
 
@@ -51,28 +51,28 @@ export default class Version extends Command {
 
     this.log(output)
 
-    return flags.verbose ?
-      versionDetail :
-      {
-        cliVersion: versionDetail.cliVersion,
-        architecture: versionDetail.architecture,
-        nodeVersion: versionDetail.nodeVersion,
-      }
+    return flags.verbose
+      ? versionDetail
+      : {
+          architecture: versionDetail.architecture,
+          cliVersion: versionDetail.cliVersion,
+          nodeVersion: versionDetail.nodeVersion,
+        }
   }
 
   private formatPlugins(plugins: Record<string, Interfaces.PluginVersionDetail>): string[] {
     return Object.entries(plugins)
-    .map(([name, plugin]) => ({name, ...plugin}))
-    .sort((a, b) => (a.name > b.name ? 1 : -1))
-    .map(plugin =>
-      `${this.getFriendlyName(plugin.name)} ${plugin.version} (${plugin.type}) ${
-        plugin.type === 'link' ? plugin.root : ''
-      }`.trim(),
-    )
+      .map(([name, plugin]) => ({name, ...plugin}))
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
+      .map((plugin) =>
+        `${this.getFriendlyName(plugin.name)} ${plugin.version} (${plugin.type}) ${
+          plugin.type === 'link' ? plugin.root : ''
+        }`.trim(),
+      )
   }
 
   private getFriendlyName(name: string): string {
-    const scope = this.config.pjson.oclif.scope
+    const {scope} = this.config.pjson.oclif
     if (!scope) return name
     const match = name.match(`@${scope}/plugin-(.+)`)
     if (!match) return name
