85f194736bd8858538321e77d54cc0b77b084228
Mike Donnalley <mdonnalley@salesforce.com>
2023-10-10 10:57:08 -0700

    feat!: migrate to ESM (#269)
    * feat!: migrate to ESM
    
    BREAKING CHANGES: node 18 minimum, removed exports
    
    * chore: remove disabled linting rules
    
    * fix: update bin scripts
    
    * chore(release): 1.3.11-qa.0
    
    ---------
    
    Co-authored-by: svc-cli-bot <svc_cli_bot@salesforce.com>
    Co-authored-by: mshanemc <shane.mclaughlin@salesforce.com>

--- test/commands/version.test.ts
+++ test/commands/version.test.ts
@@ -1,9 +1,9 @@
 import {expect, test} from '@oclif/test'
-import {type as osType, release as osRelease, userInfo as osUserInfo} from 'node:os'
-import {sep} from 'node:path'
+import {readFileSync} from 'node:fs'
+import {release as osRelease, type as osType, userInfo as osUserInfo} from 'node:os'
+import {resolve, sep} from 'node:path'
 
-// eslint-disable-next-line unicorn/prefer-module
-const pjson = require('../../package.json')
+const pjson = JSON.parse(readFileSync(resolve('package.json'), 'utf8'))
 
 const getShell = () => osUserInfo().shell?.split(sep)?.pop() || 'unknown'
 
@@ -12,57 +12,53 @@ describe('version', () => {
 `
 
   test
-  .stdout()
-  .command('version')
-  .end('runs version', output => {
-    expect(output.stdout).to.equal(stdout)
-  })
+    .stdout()
+    .command('version')
+    .end('runs version', (output) => {
+      expect(output.stdout).to.equal(stdout)
+    })
 
   test
-  .stdout()
-  .command(['version', '--verbose'])
-  .end('runs version --verbose', output => {
-    expect(output.stdout).to.contain(' CLI Version:')
-    expect(output.stdout).to.contain(`\t@oclif/plugin-version/${pjson.version}`)
-    expect(output.stdout).to.contain(' Architecture:')
-    expect(output.stdout).to.contain(`\t${process.platform}-${process.arch}`)
-    expect(output.stdout).to.contain(' Node Version:')
-    expect(output.stdout).to.contain(`\tnode-${process.version}`)
-    expect(output.stdout).to.contain(' Plugin Version:')
-    expect(output.stdout).to.contain(' OS and Version:')
-    expect(output.stdout).to.contain(`\t${osType()} ${osRelease()}`)
-    expect(output.stdout).to.contain(' Shell:')
-    expect(output.stdout).to.contain(`\t${getShell()}`)
-    expect(output.stdout).to.contain(' Root Path:')
-    expect(output.stdout).to.contain(`\t${process.cwd()}`)
-  })
+    .stdout()
+    .command(['version', '--verbose'])
+    .end('runs version --verbose', (output) => {
+      expect(output.stdout).to.contain(' CLI Version:')
+      expect(output.stdout).to.contain(`\t@oclif/plugin-version/${pjson.version}`)
+      expect(output.stdout).to.contain(' Architecture:')
+      expect(output.stdout).to.contain(`\t${process.platform}-${process.arch}`)
+      expect(output.stdout).to.contain(' Node Version:')
+      expect(output.stdout).to.contain(`\tnode-${process.version}`)
+      expect(output.stdout).to.contain(' Plugin Version:')
+      expect(output.stdout).to.contain(' OS and Version:')
+      expect(output.stdout).to.contain(`\t${osType()} ${osRelease()}`)
+      expect(output.stdout).to.contain(' Shell:')
+      expect(output.stdout).to.contain(`\t${getShell()}`)
+      expect(output.stdout).to.contain(' Root Path:')
+      expect(output.stdout).to.contain(`\t${process.cwd()}`)
+    })
 
   test
-  .stdout()
-  .command(['version', '--json'])
-  .end('runs version --json', output => {
-    const json = JSON.parse(output.stdout)
-    expect(json).to.have.keys([
-      'cliVersion',
-      'architecture',
-      'nodeVersion',
-    ])
-  })
+    .stdout()
+    .command(['version', '--json'])
+    .end('runs version --json', (output) => {
+      const json = JSON.parse(output.stdout)
+      expect(json).to.have.keys(['cliVersion', 'architecture', 'nodeVersion'])
+    })
 
   test
-  .stdout()
-  .command(['version', '--json', '--verbose'])
-  .end('runs version --verbose --json', output => {
-    const json = JSON.parse(output.stdout)
-    expect(json).to.have.property('architecture', `${process.platform}-${process.arch}`)
-    expect(json).to.have.property('cliVersion', `@oclif/plugin-version/${pjson.version}`)
-    expect(json).to.have.property('nodeVersion', `node-${process.version}`)
-    expect(json).to.have.property('osVersion', `${osType()} ${osRelease()}`)
-    expect(json).to.have.property('pluginVersions')
-    expect(json.pluginVersions).to.an('array')
-    expect(json).to.have.property('shell')
-    expect(json.shell).to.be.equal(getShell(), `json.shell: ${json.shell} getShell(): ${getShell()}`)
-    expect(json).to.have.property('rootPath', process.cwd())
-  })
+    .stdout()
+    .command(['version', '--json', '--verbose'])
+    .end('runs version --verbose --json', (output) => {
+      const json = JSON.parse(output.stdout)
+      expect(json).to.have.property('architecture', `${process.platform}-${process.arch}`)
+      expect(json).to.have.property('cliVersion', `@oclif/plugin-version/${pjson.version}`)
+      expect(json).to.have.property('nodeVersion', `node-${process.version}`)
+      expect(json).to.have.property('osVersion', `${osType()} ${osRelease()}`)
+      expect(json).to.have.property('pluginVersions')
+      expect(json.pluginVersions).to.an('array')
+      expect(json).to.have.property('shell')
+      expect(json.shell).to.be.equal(getShell(), `json.shell: ${json.shell} getShell(): ${getShell()}`)
+      expect(json).to.have.property('rootPath', process.cwd())
+    })
 })
 // {"uid":1001,"gid":123,"username":"runner","homedir":"/home/runner","shell":"/bin/bash"}
--- test/helpers/init.js
+++
@@ -1,3 +0,0 @@
-// eslint-disable-next-line unicorn/prefer-module
-const path = require('path')
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
