95f46385e4c3675dc8a59a90be5ff11c32e46113
Mike Donnalley <mdonnalley@salesforce.com>
2024-05-22 10:21:11 -0600

    test: use oclif/test v4

--- test/commands/version.test.ts
+++ test/commands/version.test.ts
@@ -1,5 +1,6 @@
-import {expect, test} from '@oclif/test'
+import {runCommand} from '@oclif/test'
 import {Ansis} from 'ansis'
+import {expect} from 'chai'
 import {readFileSync} from 'node:fs'
 import {release as osRelease, type as osType, userInfo as osUserInfo} from 'node:os'
 import {resolve, sep} from 'node:path'
@@ -11,58 +12,48 @@ const pjson = JSON.parse(readFileSync(resolve('package.json'), 'utf8'))
 const getShell = () => osUserInfo().shell?.split(sep)?.pop() || 'unknown'
 
 describe('version', () => {
-  const stdout = `@oclif/plugin-version/${pjson.version} ${process.platform}-${process.arch} node-${process.version}
-`
+  it('runs version', async () => {
+    const {stdout} = await runCommand('version')
+    expect(stdout).to.equal(
+      `@oclif/plugin-version/${pjson.version} ${process.platform}-${process.arch} node-${process.version}\n`,
+    )
+  })
 
-  test
-    .stdout()
-    .command('version')
-    .end('runs version', (output) => {
-      expect(output.stdout).to.equal(stdout)
-    })
+  it('runs version --verbose', async () => {
+    const {stdout} = await runCommand('version --verbose')
+    const stripped = ansis.strip(stdout)
+    expect(stripped).to.contain(' CLI Version:')
+    expect(stripped).to.contain(`\t@oclif/plugin-version/${pjson.version}`)
+    expect(stripped).to.contain(' Architecture:')
+    expect(stripped).to.contain(`\t${process.platform}-${process.arch}`)
+    expect(stripped).to.contain(' Node Version:')
+    expect(stripped).to.contain(`\tnode-${process.version}`)
+    expect(stripped).to.contain(' Plugin Version:')
+    expect(stripped).to.contain(' OS and Version:')
+    expect(stripped).to.contain(`\t${osType()} ${osRelease()}`)
+    expect(stripped).to.contain(' Shell:')
+    expect(stripped).to.contain(`\t${getShell()}`)
+    expect(stripped).to.contain(' Root Path:')
+    expect(stripped).to.contain(`\t${process.cwd()}`)
+  })
 
-  test
-    .stdout()
-    .command(['version', '--verbose'])
-    .end('runs version --verbose', (output) => {
-      const stdout = ansis.strip(output.stdout)
-      expect(stdout).to.contain(' CLI Version:')
-      expect(stdout).to.contain(`\t@oclif/plugin-version/${pjson.version}`)
-      expect(stdout).to.contain(' Architecture:')
-      expect(stdout).to.contain(`\t${process.platform}-${process.arch}`)
-      expect(stdout).to.contain(' Node Version:')
-      expect(stdout).to.contain(`\tnode-${process.version}`)
-      expect(stdout).to.contain(' Plugin Version:')
-      expect(stdout).to.contain(' OS and Version:')
-      expect(stdout).to.contain(`\t${osType()} ${osRelease()}`)
-      expect(stdout).to.contain(' Shell:')
-      expect(stdout).to.contain(`\t${getShell()}`)
-      expect(stdout).to.contain(' Root Path:')
-      expect(stdout).to.contain(`\t${process.cwd()}`)
-    })
+  it('runs version --json', async () => {
+    const {stdout} = await runCommand('version --json')
+    const json = JSON.parse(stdout)
+    expect(json).to.have.keys(['cliVersion', 'architecture', 'nodeVersion'])
+  })
 
-  test
-    .stdout()
-    .command(['version', '--json'])
-    .end('runs version --json', (output) => {
-      const json = JSON.parse(output.stdout)
-      expect(json).to.have.keys(['cliVersion', 'architecture', 'nodeVersion'])
-    })
-
-  test
-    .stdout()
-    .command(['version', '--json', '--verbose'])
-    .end('runs version --verbose --json', (output) => {
-      const json = JSON.parse(output.stdout)
-      expect(json).to.have.property('architecture', `${process.platform}-${process.arch}`)
-      expect(json).to.have.property('cliVersion', `@oclif/plugin-version/${pjson.version}`)
-      expect(json).to.have.property('nodeVersion', `node-${process.version}`)
-      expect(json).to.have.property('osVersion', `${osType()} ${osRelease()}`)
-      expect(json).to.have.property('pluginVersions')
-      expect(json.pluginVersions).to.an('array')
-      expect(json).to.have.property('shell')
-      expect(json.shell).to.be.equal(getShell(), `json.shell: ${json.shell} getShell(): ${getShell()}`)
-      expect(json).to.have.property('rootPath', process.cwd())
-    })
+  it('runs version --json --verbose', async () => {
+    const {stdout} = await runCommand('version --json --verbose')
+    const json = JSON.parse(stdout)
+    expect(json).to.have.property('architecture', `${process.platform}-${process.arch}`)
+    expect(json).to.have.property('cliVersion', `@oclif/plugin-version/${pjson.version}`)
+    expect(json).to.have.property('nodeVersion', `node-${process.version}`)
+    expect(json).to.have.property('osVersion', `${osType()} ${osRelease()}`)
+    expect(json).to.have.property('pluginVersions')
+    expect(json.pluginVersions).to.an('array')
+    expect(json).to.have.property('shell')
+    expect(json.shell).to.be.equal(getShell(), `json.shell: ${json.shell} getShell(): ${getShell()}`)
+    expect(json).to.have.property('rootPath', process.cwd())
+  })
 })
-// {"uid":1001,"gid":123,"username":"runner","homedir":"/home/runner","shell":"/bin/bash"}
